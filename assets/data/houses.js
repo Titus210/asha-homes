3da37b8d5ac999c91acfc4d4bff9a85fcba38fd5
Titus210 <tituskiplagat210@gmail.com>
2024-05-22 10:07:03 +0300

    feat: houses data

---
+++ assets/data/houses.js
@@ -0,0 +1,153 @@
+export   const interiors = [
+        {
+            id: 1,
+            location: "19th Ave NE, Seattle",
+            category: "Bedroom",
+            img: "../../images/bedroom-1.jpg",
+        },
+        {
+            id: 2,
+            location: "Queen Anne, Seattle",
+            category: "Living Room",
+            img: "../../images/living-room-1.jpg",
+        },
+        {
+            id: 3,
+            location: "Capitol Hill, Seattle",
+            category: "Kitchen",
+            img: "../../images/kitchen-1.jpg",
+        },
+        {
+            id: 4,
+            location: "Fremont, Seattle",
+            category: "Bathroom",
+            img: "../../images/bathroom-1.jpg",
+        },
+        {
+            id: 5,
+            location: "Ballard, Seattle",
+            category: "Balcony",
+            img: "../../images/living-room-2.jpg",
+        },
+        {
+            id: 6,
+            location: "Pearl District, Portland",
+            category: "Bedroom",
+            img: "../../images/edroom-2.jpg",
+        },
+        {
+            id: 7,
+            location: "Downtown, Portland",
+            category: "Living Room",
+            img: "../../images/living-room-2.jpg",b
+        },
+        {
+            id: 8,
+            location: "Alberta Arts District, Portland",
+            category: "Kitchen",
+            img: "../../images/kitchen-2.jpg",
+        },
+        {
+            id: 9,
+            location: "Hawthorne, Portland",
+            category: "Bathroom",
+            img: "../../images/bathroom-2.jpg",
+        },
+        {
+            id: 10,
+            location: "Sellwood, Portland",
+            category: "Balcony",
+            img: "../../images/balcony-2.jpg",
+        },
+        {
+            id: 11,
+            location: "Nob Hill, San Francisco",
+            category: "Bedroom",
+            img: "../../images/bedroom-3.jpg",
+        },
+        {
+            id: 12,
+            location: "Mission District, San Francisco",
+            category: "Living Room",
+            img: "../../images/living-room-3.jpg",
+        },
+        {
+            id: 13,
+            location: "SoMa, San Francisco",
+            category: "Kitchen",
+            img: "../../images/kitchen-3.jpg",
+        },
+        {
+            id: 14,
+            location: "Pacific Heights, San Francisco",
+            category: "Bathroom",
+            img: "../../images/bathroom-3.jpg",
+        },
+        {
+            id: 15,
+            location: "Noe Valley, San Francisco",
+            category: "Balcony",
+            img: "../../images/balcony-3.jpg",
+        },
+        {
+            id: 16,
+            location: "Beverly Hills, Los Angeles",
+            category: "Bedroom",
+            img: "../../images/bedroom-4.jpg",
+        },
+        {
+            id: 17,
+            location: "Venice, Los Angeles",
+            category: "Living Room",
+            img: "../../images/living-room-4.jpg",
+        },
+        {
+            id: 18,
+            location: "Silver Lake, Los Angeles",
+            category: "Kitchen",
+            img: "../../images/kitchen-4.jpg",
+        },
+        {
+            id: 19,
+            location: "Hollywood, Los Angeles",
+            category: "Bathroom",
+            img: "../../images/bedroom-8.jpg",
+        },
+        {
+            id: 20,
+            location: "Brentwood, Los Angeles",
+            category: "Balcony",
+            img: "../../images/balcony-3.jpg",
+        },
+        {
+            id: 21,
+            location: "Chelsea, New York City",
+            category: "Bedroom",
+            img: "../../images/bedroom-2.jpg",
+        },
+        {
+            id: 22,
+            location: "Upper East Side, New York City",
+            category: "Living Room",
+            img: "../../images/living-room-5.jpg",
+        },
+        {
+            id: 23,
+            location: "Greenwich Village, New York City",
+            category: "Kitchen",
+            img: "../../images/kitchen-4.jpg",
+        },
+        {
+            id: 24,
+            location: "Williamsburg, New York City",
+            category: "Bathroom",
+            img: "../../images/living-room-9.jpg",
+        },
+        {
+            id: 25,
+            location: "SoHo, New York City",
+            category: "Balcony",
+            img: "../../images/kitchen-4.jpg",
+        }
+    ];
+// Path: assets/data/interiors.js
